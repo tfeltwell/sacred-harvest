de731b39d5974efc0756d296905b880895c0dce9
Tom Feltwell <tfeltwell@gmail.com>
2015-01-24 15:58:05 +0000

    Added psmove path to path variable

--- controller-functions.py
+++ controller-functions.py
@@ -1,3 +1,11 @@
+import sys, os
+
+# This loads psmoave in from where it lives
+apiPath = '/home/dev/src/psmoveapi/build' # This will depend on who's running it...
+if os.path.exists(apiPath):
+    sys.path.append(apiPath)
+    print 'Set path variable for psMove '
+
 import psmove
 
 # Basic functions to Sacred Harvest to utilise PS Move controllers
@@ -7,7 +15,7 @@ import psmove
 move = psmove.PSMove()
 
 move.set_leds(0,255,0)
-move.set_rumble(255)
+# move.set_rumble(255)
 
 while True:
 	if move.poll():
