104bd260b3debaea89c74b10cb7bc9f3f0f5b473
Tom Feltwell <tfeltwell@gmail.com>
2015-01-24 16:55:00 +0000

    Main method
    Reimplemented loop to iterate through each move
    
    Left in a testing/debug method at the bottom, this will print the controller number and turn the light green of each controller when button is pressed, until all are connected. Helps you testing numbering, how many are connected etc

--- main.py
+++ main.py
@@ -5,9 +5,26 @@ import controller_functions
 if __name__ == "__main__":
 
 	handler = controller_functions.cHandler() # Init our controller handler
-	handler.setLEDs()
+
 	while True:
-		if handler.move1.poll():
-			trig = handler.move1.get_trigger()
-			print 'Trigger value:', trig
-		handler.move1.update_leds()
+		for i, (serial, move) in enumerate(sorted(handler.moves)):
+			if move.poll():
+				move.set_leds(0,255,0)
+				move.update_leds()
+				trig = move.get_trigger()
+				print '#'+str(i),' Trigger value:', trig
+
+	## DEBUG ##
+	# Test the connections, press trigger and each one should light up
+
+	# while len(handler.serials) < len(handler.moves):
+	# 	    for i, (serial, move) in enumerate(sorted(handler.moves)):
+	# 	        if serial in handler.serials:
+	# 	            continue
+
+	# 	        move.poll()
+	# 	        if move.get_buttons():
+	# 	            move.set_leds(0, 255, 0)
+	# 	            move.update_leds()
+	# 	            handler.serials.append(serial)
+	# 	            print i
